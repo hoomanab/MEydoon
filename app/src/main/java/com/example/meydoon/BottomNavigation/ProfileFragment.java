15197cd625b802f985245efcc17a7295c8590b5c
hoomanab <hooman.abasi.ric@gmail.com>
2017-02-22 18:41:35 +0330

    Profile layout almost Done!

--- app/src/main/java/com/example/meydoon/BottomNavigation/ProfileFragment.java
+++ app/src/main/java/com/example/meydoon/BottomNavigation/ProfileFragment.java
@@ -1,9 +1,26 @@
 package com.example.meydoon.BottomNavigation;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by hooma on 2/8/2017.
  */
 public class ProfileFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
 }
